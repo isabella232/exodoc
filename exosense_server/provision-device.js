33f8de40140da2ff70fac4053076f0a23bf9c67c
Magnus Feuer <magnus@feuerlabs.com>
2012-11-17 21:07:09 -0800

    Pust pes

---
+++ exosense_server/provision-device.js
@@ -0,0 +1,12 @@
+{
+  "jsonrpc": "2.0",
+  "method": "exodm:provision-device",
+  "id": "",
+  "params": 
+  {
+    "dev-id": "4711",
+    "protocol": "custom-protocol",
+    "weight": 200,
+    "color": "red"
+  }
+}
